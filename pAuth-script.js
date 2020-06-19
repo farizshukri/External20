6e9abed4834c4217ba4f5168c6b55b6f7365cbd7
Fariz Shukri Hassoun || ꧋ꦥ꦳ꦫꦶꦗ꦳꧀ꦱ꧀ꦲꦸꦏꦿꦶ <farizshukri@icloud.com>
2020-06-19 18:20:30 +0800

    initial commit

---
+++ pAuth-script.js
@@ -0,0 +1,54 @@
+// frontend/script.js
+document.addEventListener('DOMContentLoaded', () => {
+    const loginForm = document.getElementById('login-form');
+    const registerForm = document.getElementById('register-form');
+    const errorMessage = document.getElementById('error-message');
+    const registerMessage = document.getElementById('register-message');
+
+    loginForm.addEventListener('submit', (e) => {
+        e.preventDefault();
+        const username = document.getElementById('username').value;
+        const password = document.getElementById('password').value;
+
+        fetch('http://localhost:5000/api/auth/login', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({ username, password })
+        })
+        .then(response => response.json())
+        .then(data => {
+            if (data.token) {
+                localStorage.setItem('token', data.token);
+                window.location.href = '/dashboard.html'; // Redirect to a dashboard or another page
+            } else {
+                errorMessage.textContent = 'Invalid credentials';
+            }
+        })
+        .catch(error => {
+            errorMessage.textContent = 'An error occurred';
+        });
+    });
+
+    registerForm.addEventListener('submit', (e) => {
+        e.preventDefault();
+        const username = document.getElementById('register-username').value;
+        const password = document.getElementById('register-password').value;
+
+        fetch('http://localhost:5000/api/auth/register', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({ username, password })
+        })
+        .then(response => response.json())
+        .then(data => {
+            registerMessage.textContent = data.message || 'Registration successful';
+        })
+        .catch(error => {
+            registerMessage.textContent = 'An error occurred';
+        });
+    });
+});
