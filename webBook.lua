da4b02ebf148d439e8038aa15d5fcb9b026d59e6
Fariz Shukri Hassoun || ꧋ꦥ꦳ꦫꦶꦗ꦳꧀ꦱ꧀ꦲꦸꦏꦿꦶ <farizshukri@icloud.com>
2020-08-06 18:20:30 +0800

    initial commit

---
+++ webBook.lua
@@ -0,0 +1,16 @@
+books-website/
+├── backend/
+│   ├── app.js
+│   ├── routes/
+│   │   ├── books.js
+│   ├── models/
+│   │   └── Book.js
+│   ├── config/
+│   │   └── db.js
+│   └── package.json
+├── frontend/
+│   ├── index.html
+│   ├── add-book.html
+│   ├── style.css
+│   └── script.js
+└── README.md
