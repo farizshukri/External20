2ec227aff59cbb8a4403f8943522678ef816b219
Fariz Shukri Hassoun || ꧋ꦥ꦳ꦫꦶꦗ꦳꧀ꦱ꧀ꦲꦸꦏꦿꦶ <farizshukri@icloud.com>
2020-09-02 18:20:30 +0800

    initial commit

---
+++ bixz.lua
@@ -0,0 +1,19 @@
+business-website/
+├── backend/
+│   ├── app.js
+│   ├── routes/
+│   │   └── contact.js
+│   ├── models/
+│   │   └── Message.js
+│   ├── config/
+│   │   └── db.js
+│   └── package.json
+├── frontend/
+│   ├── index.html
+│   ├── services.html
+│   ├── contact.html
+│   ├── style.css
+│   ├── script.js
+│   └── images/
+│       └── logo.png
+└── README.md
