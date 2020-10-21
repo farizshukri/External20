dd4ab12fe2b6118bdf68c668265317ab14403896
Fariz Shukri Hassoun || ꧋ꦥ꦳ꦫꦶꦗ꦳꧀ꦱ꧀ꦲꦸꦏꦿꦶ <farizshukri@icloud.com>
2020-10-21 18:20:30 +0800

    initial commit

---
+++ urlCut.lua
@@ -0,0 +1,15 @@
+link-shortener/
+├── backend/
+│   ├── app.js
+│   ├── routes/
+│   │   └── links.js
+│   ├── models/
+│   │   └── Link.js
+│   ├── config/
+│   │   └── db.js
+│   └── package.json
+├── frontend/
+│   ├── index.html
+│   ├── style.css
+│   └── script.js
+└── README.md
