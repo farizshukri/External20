3033e77711f5f13b2a6863b200b9c30f4dfd53d4
Fariz Shukri Hassoun || ꧋ꦥ꦳ꦫꦶꦗ꦳꧀ꦱ꧀ꦲꦸꦏꦿꦶ <farizshukri@icloud.com>
2020-12-24 18:20:30 +0800

    initial commit

---
+++ webBook-modelsBook.js
@@ -0,0 +1,11 @@
+// backend/models/Book.js
+const mongoose = require('mongoose');
+
+const BookSchema = new mongoose.Schema({
+    title: { type: String, required: true },
+    author: { type: String, required: true },
+    year: { type: Number, required: true },
+    genre: { type: String, required: true }
+});
+
+module.exports = mongoose.model('Book', BookSchema);
